8ee04fa5a22fbb99c263379bdac0d5cfdea3bfd4
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2022-05-11 21:39:34 +0530

    component added

---
+++ src/Components/Sidebar.js
@@ -0,0 +1,31 @@
+import React, {useEffect, useState} from 'react'
+import Channel from './Channel'
+const Sidebar = () => {
+    const [articles, setArticles] = useState([])
+    let list = []
+    const updateNews = async ()=>{
+        // const url = `https://gnews.io/api/v4/search?q=${props.category}&token=${props.apiKey}&lang=hi`;
+        const url = "http://localhost:3000/c.json";
+        let data = await fetch(url);
+        let parsedData = await data.json()
+        setArticles(parsedData);
+    }
+    useEffect(() => {
+        updateNews();
+        //eslint-disable-next-line
+    }, [])
+    return (
+        <>
+        
+           <div class="w3-sidebar w3-bar-block w3-light-grey w3-card" style={{width:"15%"}}>
+           {articles.map((element) => {
+                            return <>
+                                <Channel id={element.id}  name={element.name}/>
+                                </>
+                        })}
+            </div>
+        </>
+    )
+}
+
+export default Sidebar
